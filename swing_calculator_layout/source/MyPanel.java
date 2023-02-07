e7809798fbc22c5eccf3a443e8efe3a6709d313e
Qingyan Yang <100550092+qingyanyang@users.noreply.github.com>
2023-02-07 23:04:43 +1030

    Delete swing_calculator_layout directory

--- swing_calculator_layout/source/MyPanel.java
+++
@@ -1,9 +0,0 @@
-
-import javax.swing.*;
-public class MyPanel extends JPanel {
-    public MyPanel(int x, int y, int w, int h){
-        this.setBounds(x,y,w,h);
-        this.setBackground(null);
-        this.setOpaque(false);
-    }
-}
